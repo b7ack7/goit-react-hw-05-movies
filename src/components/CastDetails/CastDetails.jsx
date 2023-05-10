fb38b6327dbf07095de95beaa0029b4d15072eb0
Maksym Vasiliev <makskremen74@gmail.com>
2023-05-10 11:07:54 +0300

    fix

--- src/components/CastDetails/CastDetails.jsx
+++ src/components/CastDetails/CastDetails.jsx
@@ -5,12 +5,11 @@ const IMAGES_BASE_URL = 'https://image.tmdb.org/t/p/w200/';
 export const CastDetails = ({ profile_path, original_name, character }) => {
   return (
     <li>
-      {profile_path && (
-        <img
-          src={profile_path ? IMAGES_BASE_URL + profile_path : DefaultImage}
-          alt={original_name}
-        />
-      )}
+      <img
+        src={profile_path ? IMAGES_BASE_URL + profile_path : DefaultImage}
+        alt={original_name}
+      />
+
       <h4>{original_name}</h4>
       <p>Character: {character || 'No information'}</p>
     </li>
--- src/components/MovieInfo/MovieInfo.jsx
+++ src/components/MovieInfo/MovieInfo.jsx
@@ -1,24 +1,36 @@
 import PropTypes from 'prop-types';
 import { Wrapper } from './MovieInfo.styled';
-
+import DefaultImage from 'components/DefaultImage';
 const IMAGES_BASE_URL = 'https://image.tmdb.org/t/p/w200/';
 
-export const MovieInfo = ({poster_path, title, release_date, vote_average, overview, genres}) => {
-    return (
+export const MovieInfo = ({
+  poster_path,
+  title,
+  release_date,
+  vote_average,
+  overview,
+  genres,
+}) => {
+  return (
     <Wrapper>
-      {poster_path && <img src={IMAGES_BASE_URL + poster_path} alt={title} />}
+      <img
+        src={poster_path ? IMAGES_BASE_URL + poster_path : DefaultImage}
+        alt={title}
+      />
+
       <div>
-      <h2>{title} ({release_date?.slice(0, 4)})</h2>
-      <p>User Score: {Math.round(vote_average * 10)}%</p>
-      <h3>Overview</h3>
-      <p>{overview}</p>
-      <h3>Genres</h3>
-      <p>{genres?.map(genre => genre.name).join(" ")}</p>
+        <h2>
+          {title} ({release_date?.slice(0, 4)})
+        </h2>
+        <p>User Score: {Math.round(vote_average * 10)}%</p>
+        <h3>Overview</h3>
+        <p>{overview}</p>
+        <h3>Genres</h3>
+        <p>{genres?.map(genre => genre.name).join(' ')}</p>
       </div>
-    </Wrapper>  
-        
-    )
-}
+    </Wrapper>
+  );
+};
 
 MovieInfo.propTypes = {
   poster_path: PropTypes.string,
@@ -26,7 +38,9 @@ MovieInfo.propTypes = {
   release_date: PropTypes.string,
   vote_average: PropTypes.number,
   overview: PropTypes.string,
-  genres: PropTypes.arrayOf(PropTypes.shape({
-    name: PropTypes.string
-  }))
-}
+  genres: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string,
+    })
+  ),
+};
